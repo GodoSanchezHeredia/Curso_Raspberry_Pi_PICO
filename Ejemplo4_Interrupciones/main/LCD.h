9b6c80883c37aae2ff554bc14a23255004168ef3
Godo <139667276+GodoSanchezHeredia@users.noreply.github.com>
2024-03-25 21:16:44 -0500

    V1

---
+++ Ejemplo4_Interrupciones/main/LCD.h
@@ -0,0 +1,84 @@
+/*
+ * LCD.h
+ *
+ *  Created on: Sep 23, 2023
+ *      Author: DELL
+ */
+
+#ifndef INC_LCD_H_
+#define INC_LCD_H_
+
+
+#include "pico/stdlib.h"
+
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#define E_ON   	gpio_put(11,1);
+#define E_OFF  	gpio_put(11,0);
+
+#define RS_ON 	gpio_put(12,1);
+#define RS_OFF 	gpio_put(12,0);
+
+#define D4_ON 	gpio_put(10,1);
+#define D4_OFF 	gpio_put(10,0);
+
+#define D5_ON 	gpio_put(9,1);
+#define D5_OFF  gpio_put(9,0);
+
+#define D6_ON 	gpio_put(8,1);
+#define D6_OFF 	gpio_put(8,0);
+
+#define D7_ON 	gpio_put(7,1);
+#define D7_OFF 	gpio_put(7,0);
+
+typedef enum{
+	fila1=0,
+	fila2,
+	fila3,
+	fila4
+}Ubicacion;
+
+#include "pico/stdlib.h"
+
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#define E_ON   	gpio_put(11,1);
+#define E_OFF  	gpio_put(11,0);
+
+#define RS_ON 	gpio_put(12,1);
+#define RS_OFF 	gpio_put(12,0);
+
+#define D4_ON 	gpio_put(10,1);
+#define D4_OFF 	gpio_put(10,0);
+
+#define D5_ON 	gpio_put(9,1);
+#define D5_OFF  gpio_put(9,0);
+
+#define D6_ON 	gpio_put(8,1);
+#define D6_OFF 	gpio_put(8,0);
+
+#define D7_ON 	gpio_put(7,1);
+#define D7_OFF 	gpio_put(7,0);
+
+
+
+void LCD_PICO_CMD(uint8_t a);
+void LCD_PICO_BUS(uint8_t a);
+void LCD_PICO_INIT(void);
+void LCD_PICO_SET_CURSOR(uint8_t x,Ubicacion y);
+void LCD_PICO_PRINT_CHAR(char a);
+void LCD_PICO_PRINT_STRING(char *a);
+void LCD_PICO_New_Char(uint8_t a,uint8_t b,uint8_t c,uint8_t d ,uint8_t e ,uint8_t f,uint8_t g,uint8_t h,uint8_t i);
+void LCD_PICO_Print_New_Char(uint8_t a);
+void LCD_PICO_Clear(void);
+void LCD_PICO_Home(void);
+
+
+
+#endif /* INC_LCD_H_ */
--- Ejemplo4_Interrupciones/main/main.c
+++ Ejemplo4_Interrupciones/main/main.c
@@ -1,19 +1,67 @@
 
 
 #include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include "stdint.h"
+#include "stdbool.h"
+#include "LCD.h"
+
+
+volatile uint8_t count=0;
+volatile bool flag = false;
+
+char str_count[15];
+void ISR_InterruptExtern(uint gpio);
 
 int main() {
-#ifndef PICO_DEFAULT_LED_PIN
-#warning blink example requires a board with a regular LED
-#else
-    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
-    while (true) {
-        gpio_put(LED_PIN, 1);
-        sleep_ms(100);
-        gpio_put(LED_PIN, 0);
-        sleep_ms(100);
+
+
+LCD_PICO_INIT();
+LCD_PICO_SET_CURSOR(0,fila1);
+LCD_PICO_PRINT_STRING("Clase Interrup");
+
+gpio_init(16);
+gpio_set_dir(16,GPIO_IN);
+gpio_pull_up(16);
+gpio_set_irq_enabled_with_callback(16,GPIO_IRQ_EDGE_FALL,true,&ISR_InterruptExtern);
+
+gpio_init(17);
+gpio_set_dir(17,GPIO_IN);
+gpio_pull_down(17);
+gpio_set_irq_enabled_with_callback(17,GPIO_IRQ_EDGE_RISE,true,&ISR_InterruptExtern);
+
+
+
+
+for (;;)
+{
+    /* code */
+    if(flag){
+
+        LCD_PICO_SET_CURSOR(0,fila2);
+        sprintf(str_count,"Count:%03u",count);
+        LCD_PICO_PRINT_STRING(str_count);
+
+
+        flag=false;
     }
-#endif
 }
+
+
+}
+
+
+void ISR_InterruptExtern(uint gpio){
+
+    if (gpio==16)
+    {
+        count++;
+    }
+        if (gpio==17)
+    {
+        count--;
+    }
+    
+    flag = true;
+}
+
